47b9a9d12ea9e6b3af0c353daace44a0baf84d0e
Waqqas Sharif <vickyrare@gmail.com>
2021-07-11 20:26:50 +1000

    cleanup

--- src/main/java/io/codecrafts/rest/CommandController.java
+++ src/main/java/io/codecrafts/rest/CommandController.java
@@ -6,7 +6,6 @@ import io.codecrafts.rest.annotations.RestApiController;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
